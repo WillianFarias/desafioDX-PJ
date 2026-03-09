c08bd6322d63cb5f05a5caf159d307789d498dc9
Willian Farias <williansousafarias@gmail.com>
2026-03-09 12:38:33 -0300

    fix: importar RouterModule no app component para ativar navegação via routerLink

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterModule   // NECESSÁRIO PARA routerLink FUNCIONAR!
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
-}
+export class AppComponent {}
