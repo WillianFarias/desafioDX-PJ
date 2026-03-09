4234652239e27928fbcb00743eb1468e6b3bace4
Willian Farias <williansousafarias@gmail.com>
2026-03-09 09:01:31 -0300

    feat: implementacao metodo integranteMaisUsado e metodo auxiliar filtrarPorData

--- src/main/java/br/com/duxusdesafio/service/ApiService.java
+++ src/main/java/br/com/duxusdesafio/service/ApiService.java
@@ -1,5 +1,6 @@
 package br.com.duxusdesafio.service;
 
+import br.com.duxusdesafio.model.ComposicaoTime;
 import br.com.duxusdesafio.model.Integrante;
 import br.com.duxusdesafio.model.Time;
 import org.springframework.stereotype.Service;
@@ -7,10 +8,12 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Service que possuirá as regras de negócio para o processamento dos dados solicitados no desafio!
  */
+@Service
 public class ApiService {
 
     /**
@@ -28,8 +31,16 @@ public class ApiService {
      * dentro do período
      */
     public Integrante integranteMaisUsado(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes){
-        // TODO Implementar método seguindo as instruções!
-        return null;
+        List<Time> filtrados = filtrarPorData(dataInicial, dataFinal, todosOsTimes);
+
+        return filtrados.stream()
+            .flatMap(t -> t.getComposicoes().stream())
+            .map(ComposicaoTime::getIntegrante)
+            .collect(Collectors.groupingBy(i -> i, Collectors.counting()))
+            .entrySet().stream()
+            .max(Map.Entry.comparingByValue())
+            .map(Map.Entry::getKey)
+            .orElse(null);
     }
 
     /**
@@ -74,4 +85,11 @@ public class ApiService {
         return null;
     }
 
+    // Métodos auxiliares privados
+    private List<Time> filtrarPorData(LocalDate inicio, LocalDate fim, List<Time> times) {
+        return times.stream()
+                .filter(t -> (inicio == null || !t.getData().isBefore(inicio)) &&
+                        (fim == null || !t.getData().isAfter(fim)))
+                .collect(Collectors.toList());
+    }
 }
