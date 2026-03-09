fdc570f590ccbde2f2546d70c6b57752540e2b66
Willian Farias <williansousafarias@gmail.com>
2026-03-09 09:05:53 -0300

    feat: implementacao metodo contagemPorFranquia

--- src/main/java/br/com/duxusdesafio/service/ApiService.java
+++ src/main/java/br/com/duxusdesafio/service/ApiService.java
@@ -99,13 +99,14 @@ public class ApiService {
             .orElse(null);
     }
 
-
     /**
      * Vai retornar o número (quantidade) de Franquias dentro do período
      */
     public Map<String, Long> contagemPorFranquia(LocalDate dataInicial, LocalDate dataFinal, List<Time> todosOsTimes){
-        // TODO Implementar método seguindo as instruções!
-        return null;
+        return filtrarPorData(dataInicial, dataFinal, todosOsTimes).stream()
+            .flatMap(t -> t.getComposicoes().stream())
+            .map(c -> c.getIntegrante().getFranquia())
+            .collect(Collectors.groupingBy(f -> f, Collectors.counting()));
     }
 
     /**
